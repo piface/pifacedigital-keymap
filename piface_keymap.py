d0405907155d1a1715a08eaeee19ed3f20cf7fe4
Thomas Preston <thomasmarkpreston@gmail.com>
2012-08-22 11:18:41 +0100

    updated piface keymap to hold button state in booleans

--- piface_keymap.py
+++ piface_keymap.py
@@ -54,50 +54,63 @@ def main():
             # second input pressed
             second_key_pressed = True
             device.emit(SECOND_KEY, 1) # Press
-        else:
+        elif second_key_pressed:
             # second input not pressed
+            second_key_pressed = False
             device.emit(SECOND_KEY, 0) # Release
 
-        if input_bitp & 0x4:
+        if (input_bitp & 0x4) and not third_key_pressed:
             # third input pressed
+            third_key_pressed = True
             device.emit(THIRD_KEY, 1) # Press
-        else:
+        elif third_key_pressed:
             # third input not pressed
+            third_key_pressed = False
             device.emit(THIRD_KEY, 0) # Release
 
-        if input_bitp & 0x8:
+        if (input_bitp & 0x8) and not fourth_key_pressed:
             # fourth input pressed
+            fourth_key_pressed = True
             device.emit(FOURTH_KEY, 1) # Press
-        else:
+        elif fourth_key_pressed:
             # fourth input not pressed
+            fourth_key_pressed = False
             device.emit(FOURTH_KEY, 0) # Release
 
-        if input_bitp & 0x10:
+        if (input_bitp & 0x10) and not fifth_key_pressed:
             # fifth input pressed
+            fifth_key_pressed = True
             device.emit(FIFTH_KEY, 1) # Press
-        else:
+        elif fifth_key_pressed:
             # fifth input not pressed
+            fifth_key_pressed = False
             device.emit(FIFTH_KEY, 0) # Release
 
-        if input_bitp & 0x20:
+        if (input_bitp & 0x20) and not sixth_key_pressed:
             # sixth input pressed
+            sixth_key_pressed = True
             device.emit(SIXTH_KEY, 1) # Press
-        else:
+        elif sixth_key_pressed:
             # sixth input not pressed
+            sixth_key_pressed = False
             device.emit(SIXTH_KEY, 0) # Release
 
-        if input_bitp & 0x40:
+        if (input_bitp & 0x40) and not seventh_key_pressed:
             # seventh input pressed
+            seventh_key_pressed = True
             device.emit(SEVENTH_KEY, 1) # Press
-        else:
+        elif seventh_key_pressed:
             # seventh input not pressed
+            seventh_key_pressed = False
             device.emit(SEVENTH_KEY, 0) # Release
 
-        if input_bitp & 0x80:
+        if (input_bitp & 0x80) and not eighth_key_pressed:
             # eighth input pressed
+            eighth_key_pressed = True
             device.emit(EIGHTH_KEY, 1) # Press
-        else:
+        elif eighth_key_pressed:
             # eighth input not pressed
+            eighth_key_pressed = False
             device.emit(EIGHTH_KEY, 0) # Release
 
 if __name__ == "__main__":
